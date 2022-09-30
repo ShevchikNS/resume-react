9919f22458eef3bc6c3c1dacf760f1572ac5e3e4
ABC Man <Shevchikkola@gmail.com>
2022-09-30 14:23:54 +0300

    fix

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -19,14 +19,13 @@ const ContactPage = () => {
                     <input type="hidden" name="_subject" value="New mail from Portfolio page!"/>
                     <TextField type="text" name="name" placeholder="Your name" required/>
                     <TextField type="email" name="email" placeholder="Email Address" required/>
-                    <input type="hidden" name="_next" value="https://shevchik-resume.netlify.app/ThankYou"/>
+                    <input type="hidden" name="_next" value="https://thank-page.vercel.app/"/>
                     <input type="hidden" name="_captcha" value="false"/>
                 </div>
 
                 <div className="TextArea">
                     <TextField type="text" name="message" placeholder="Your message" required />
                     <Button type="submit" >Submit</Button>
-                    <Button onClick={() => navigate('/ThankYou')} >123</Button>
 
                 </div>
             </form>
