c80446e9ac7849068ddd05297b494eeb6b004183
ABC Man <Shevchikkola@gmail.com>
2022-09-30 11:02:45 +0300

    fix

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import './ContactPage.css'
 import {TextField} from "@mui/material";
 import Button from "@mui/material/Button";
-import {useNavigate} from "react-router-dom";
+// import {useNavigate} from "react-router-dom";
 
 const ContactPage = () => {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     return (
         <div className="ContactPage" id="Contact">
