63387fb6091d3467dc831f82d15f02c9c884cf73
ABC Man <Shevchikkola@gmail.com>
2022-10-05 15:22:34 +0300

    change text about Me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,9 +6,9 @@ const About = () => {
             <h1 className="AboutTitle">About Me</h1>
             <p className="AboutTitle2">
                 About me.I graduated from Yanka Kupala Grodno State University, Faculty of Mathematics and Informatics.
-                During his studies, he took a course in React at Cogniteq.
-                After graduating from university, he got a job as a software engineer at Khimvolokno.
-                From January 2022 to April 2022, he took React courses from Intesoft. At the moment I am developing and supporting programs in the 1C language,
+                When I was studying, I completed courses in React at Cogniteq.
+                After graduating from university, I got a job as a software engineer at Khimvolokno.
+                From January 2022 to April 2022, I took React courses from Intesoft. At the moment I am developing and supporting programs in the 1C language,
                 advising users on the ERP system, writing instructions for the 1C system, and also compiling reports on the errors found.
                 At the same time, I continue to study React and other technologies.
             </p>
@@ -23,8 +23,6 @@ const About = () => {
                 <li>Redux</li>
                 <li>Git</li>
                 <li>1C</li>
-
-
             </ul>
         </div>
     );
